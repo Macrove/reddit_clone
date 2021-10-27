ea292b997772af827f5d665acafff9e4c3daf639
Macrove <64074771+Macrove@users.noreply.github.com>
2021-10-28 00:58:39 +0530

    not able to load graphql schema BUG

---
+++ server/src/types.ts
@@ -0,0 +1,13 @@
+import { EntityManager, Connection, IDatabaseDriver } from "@mikro-orm/core";
+import { Request, Response } from 'express'
+
+declare module 'express-session' {
+    interface Session {
+        userId: number;
+    }
+}
+export type MyContext = {
+    em: EntityManager<IDatabaseDriver<Connection>>
+    req: Request
+    res: Response
+}
---
+++ web/with-chakra-ui-app/src/pages/register.tsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Form, Formik } from 'formik'
+import { Wrapper } from '../components/Wrapper'
+import { InputField } from '../components/InputField'
+import { Box } from '@chakra-ui/layout'
+import { Button } from '@chakra-ui/button'
+import { useMutation } from 'urql'
+
+interface registerProps {
+
+}
+
+const RegisterMut = `
+mutation Register($username : String!, $password : String!){
+    register(options : {username : $username, password : $password}) {
+      errors {
+        field
+        message
+      }
+      user {
+        id
+        username
+      }
+    }
+  }
+`
+const Register: React.FC<registerProps> = ({ }) => {
+    const [, register] = useMutation(RegisterMut)
+    return <Wrapper variant="small">
+        <Formik initialValues={{ username: "", password: "" }}
+            onSubmit={async (values) => {
+                console.log(values)
+                const response = await register(values)
+            }}
+        >
+            {({ isSubmitting }) => (
+                <Form>
+                    <InputField
+                        name="username"
+                        placeholder="username"
+                        label="Username"
+                    />
+
+                    <Box mt={4}>
+                        <InputField
+                            name="password"
+                            placeholder="password"
+                            label="Password"
+                            type="password"
+                        />
+                    </Box>
+                    <Button type="submit" mt={4} colorScheme="teal" isLoading={isSubmitting}>Register</Button>
+                </Form>
+            )}
+        </Formik>
+    </Wrapper>
+
+}
+
+export default Register
