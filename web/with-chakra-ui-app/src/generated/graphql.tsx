71025c14d1adec8567596532009de9d087d36ba0
Macrove <harshitraj.33@gmail.com>
2021-10-29 02:19:41 +0530

    working piece

--- web/with-chakra-ui-app/src/generated/graphql.tsx
+++ web/with-chakra-ui-app/src/generated/graphql.tsx
@@ -1,12 +1,10 @@
-import { gql } from '@apollo/client';
-import * as Apollo from '@apollo/client';
 import gql from 'graphql-tag';
 import * as Urql from 'urql';
 export type Maybe<T> = T | null;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
 export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
 export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
-const defaultOptions =  {}
+const defaultOptions = {}
 export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
@@ -106,48 +104,6 @@ export type RegisterMutationVariables = Exact<{
 
 export type RegisterMutation = { __typename?: 'Mutation', register: { __typename?: 'UserResponse', errors?: Array<{ __typename?: 'FieldError', field: string, message: string }> | null | undefined, user?: { __typename?: 'User', id: number, username: string } | null | undefined } };
 
-
-export const RegisterDocument = gql`
-    mutation Register($username: String!, $password: String!) {
-  register(options: {username: $username, password: $password}) {
-    errors {
-      field
-      message
-    }
-    user {
-      id
-      username
-    }
-  }
-}
-    `;
-export type RegisterMutationFn = Apollo.MutationFunction<RegisterMutation, RegisterMutationVariables>;
-
-/**
- * __useRegisterMutation__
- *
- * To run a mutation, you first call `useRegisterMutation` within a React component and pass it any options that fit your needs.
- * When your component renders, `useRegisterMutation` returns a tuple that includes:
- * - A mutate function that you can call at any time to execute the mutation
- * - An object with fields that represent the current status of the mutation's execution
- *
- * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
- *
- * @example
- * const [registerMutation, { data, loading, error }] = useRegisterMutation({
- *   variables: {
- *      username: // value for 'username'
- *      password: // value for 'password'
- *   },
- * });
- */
-export function useRegisterMutation(baseOptions?: Apollo.MutationHookOptions<RegisterMutation, RegisterMutationVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useMutation<RegisterMutation, RegisterMutationVariables>(RegisterDocument, options);
-      }
-export type RegisterMutationHookResult = ReturnType<typeof useRegisterMutation>;
-export type RegisterMutationResult = Apollo.MutationResult<RegisterMutation>;
-export type RegisterMutationOptions = Apollo.BaseMutationOptions<RegisterMutation, RegisterMutationVariables>;
 
 export const RegisterDocument = gql`
     mutation Register($username: String!, $password: String!) {
--- web/with-chakra-ui-app/src/pages/register.tsx
+++ web/with-chakra-ui-app/src/pages/register.tsx
@@ -3,36 +3,24 @@ import { Form, Formik } from 'formik'
 import { Wrapper } from '../components/Wrapper'
 import { InputField } from '../components/InputField'
 import { Box } from '@chakra-ui/layout'
-import { Button } from '@chakra-ui/button'
-import { useMutation } from 'urql'
+import { Button } from '@chakra-ui/button';
+import { useRegisterMutation } from './../generated/graphql';
 
 interface registerProps {
 
 }
 
-const RegisterMutation = `
-
-mutation Register($username : String!, $password : String!){
-    register(options : {username : $username, password : $password}) {
-      errors {
-        field
-        message
-      }
-      user {
-        id
-        username
-      }
-    }
-  }
-
-`
 const Register: React.FC<registerProps> = ({ }) => {
-    const [, register] = useMutation(RegisterMutation)
+    const [, register] = useRegisterMutation();
     return <Wrapper variant="small">
         <Formik initialValues={{ username: "", password: "" }}
-            onSubmit={async (values) => {
-                console.log(values)
-                const response = await register(values)
+            onSubmit={async (values, { setErrors }) => {
+                const response = await register(values);
+                if (response.data?.register.errors) {
+                    setErrors({
+                        username: "errorr!!!"
+                    })
+                }
             }}
         >
             {({ isSubmitting }) => (
